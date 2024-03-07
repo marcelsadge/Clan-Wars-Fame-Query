63567c813ae2fda16c39ad938607981af1aa365c
marcelsadge <zjunhao@seas.upenn.edu>
2024-03-07 11:09:55 -0500

    Procfile changes

--- Procfile
+++ Procfile
@@ -1,1 +1,2 @@
-web: npm run dev
+web: npm i && npm run client
+server: npm i && npm run server
--- src/server.js
+++ src/server.js
@@ -15,10 +15,6 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '../public/index.html'));
-});
-
 app.use('/fame', fameCutoffRoute);
 app.use('/get', moeRoute);
 
